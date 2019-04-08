69336b77fe91357c4c27b9406ec33365e9bbd5c8
latwood <pianotocador@gmail.com>
2019-04-08 00:22:10 -0600

    verified buildEverything_Aeolus.sh is working, though now I'm stuck trying to get the right packages to load and writing proper directory locations for cmake lists. At least farsite compiles without issues. I'm half tempted to try compiling on Ubuntu, copying and pasting onto Aeolus, then modifying paths. If I have too much trouble with module load stuff, will probably just compile 3rd party libs from source, though without apt-get the style of packages is much harder to implement. Also need to figure out how to do sudo correctly on Aeolus.

--- include/buildEverything_Aeolus.sh
+++ include/buildEverything_Aeolus.sh
@@ -5,19 +5,19 @@
 #PBS -V
 
 ## Define a job name
-#PBS -N tryComboScript
+#PBS -N compileComboScript
 
 ## Define compute options
-#PBS -l nodes=1:dev:ppn=1
-#PBS -l mem=1024mb
-#PBS -l walltime=00:05:00
+#PBS -l nodes=1:dev:ppn=4
+#PBS -l mem=2048mb
+#PBS -l walltime=00:15:00
 #PBS -q batch
 
 ## Define path for output & error logs
-#PBS -k oe
-####PBS -j oe
-#PBS -e /fastscratch/latwood/qsubLogs/tryComboScript.e
-#PBS -o /fastscratch/latwood/qsubLogs/tryComboScript.o
+####PBS -k oe
+#PBS -j oe
+####PBS -e /fastscratch/latwood/qsubLogs/tryComboScript.e
+####PBS -o /fastscratch/latwood/qsubLogs/tryComboScript.o
 
 ## Define path for reporting
 #PBS -M pianotocador@gmail.com
@@ -50,7 +50,7 @@ nCores="8"  # this is the attempted number of cores when running any make proces
 
 
 
-outputDir="/fastscratch/latwood/src"
+outputDir="/home/latwood/src"
 echo "" # add extra line
 echo "outputDir = \""$outputDir"\""
 
@@ -58,7 +58,7 @@ echo "outputDir = \""$outputDir"\""
 comboScriptLink="https://github.com/latwood/WRF-WindNinja-FarsiteScript.git"
 
 baseDir=$outputDir"/WRF-WindNinja-FarsiteScript"
-comboScriptBuildDir=$baseDir"/build"
+finalBuildDir=$baseDir"/build"
 includeDir=$baseDir"/include"
 extraAppsDir=$includeDir"/extraApps"
 
@@ -75,87 +75,165 @@ farsiteLink="https://github.com/firelab/farsite.git"
 farsiteSrcDir=$farsiteDir"/src"
 
 
+### set boolean to stop process in the middle
+success=0
+
 
 ### first create all the required source directories and build directories
-
-echo "" # add extra line
-if [ ! -d "${baseDir}" ]; then
-	echo "Entering outputDir \""$outputDir"\""
-	cd $outputDir
-	echo "running git clone on WRF-WindNinja-FarsiteScript \""$comboScriptLink"\""
-	git clone $comboScriptLink
-else
-	echo "WRF-WindNinja-FarsiteScript base dir \""$baseDir"\" already exists. Skipping repo cloning step for WRF-WindNinja-FarsiteScript"
+if [ $success == 0 ]; then
+	echo "" # add extra line
+	if [ ! -d "${baseDir}" ]; then
+		echo "Entering outputDir \""$outputDir"\""
+		cd $outputDir
+		echo "running git clone on WRF-WindNinja-FarsiteScript \""$comboScriptLink"\""
+		git clone $comboScriptLink
+		success=$?
+		if [ $success != 0 ]; then
+			echo "!!! error running git clone !!!"
+			success=1
+		fi
+	else
+		echo "WRF-WindNinja-FarsiteScript base dir \""$baseDir"\" already exists. Skipping repo cloning step for WRF-WindNinja-FarsiteScript"
+	fi
 fi
 
-echo "" # add extra line
-if [ ! -d "${extraAppsDir}" ]; then
-	echo "creating extraAppsDir \""$extraAppsDir"\""
-	mkdir $extraAppsDir
-else
-	echo "extraAppsDir \""$extraAppsDir"\" already exists. Skipping extraAppsDir creation step"
+if [ $success == 0 ]; then
+	echo "" # add extra line
+	if [ ! -d "${extraAppsDir}" ]; then
+		echo "creating extraAppsDir \""$extraAppsDir"\""
+		mkdir $extraAppsDir
+		success=$?
+		if [ $success != 0 ]; then
+			echo "!!! error running mkdir !!!"
+			success=1
+		fi
+	else
+		echo "extraAppsDir \""$extraAppsDir"\" already exists. Skipping extraAppsDir creation step"
+	fi
 fi
 
-echo "" # add extra line
-if [ ! -d "${windNinjaDir}" ]; then
-	echo "entering \""$extraAppsDir"\""
-	cd $extraAppsDir
-	echo "running git clone on WindNinja \""$windNinjaLink"\""
-	git clone $WindNinjaLink
-	echo "entering WindNinja directory"
-	cd windninja
-	echo "changing WindNinja branch to hopefully stable "$windNinjaBranch" branch"
-	git checkout $windNinjaBranch
-else
-	echo "windNinjaDir \""$windNinjaDir"\" already exists. Skipping repo cloning step for WindNinja"
+if [ $success == 0 ]; then
+	echo "" # add extra line
+	if [ ! -d "${windNinjaDir}" ]; then
+		echo "entering \""$extraAppsDir"\""
+		cd $extraAppsDir
+		echo "running git clone on WindNinja \""$windNinjaLink"\""
+		git clone $windNinjaLink
+		success=$?
+		if [ $success != 0 ]; then
+			echo "!!! error running git clone !!!"
+			success=1
+		else
+			echo "entering WindNinja directory"
+			cd windninja
+			echo "changing WindNinja branch to hopefully stable "$windNinjaBranch" branch"
+			git checkout $windNinjaBranch
+			success=$?
+			if [ $success != 0 ]; then
+				echo "!!! error running git checkout !!!"
+				success=1
+			fi
+		fi
+	else
+		echo "windNinjaDir \""$windNinjaDir"\" already exists. Skipping repo cloning step for WindNinja"
+	fi
 fi
 
-echo "" # add extra line
-if [ ! -d "${farsiteDir}" ]; then
-	echo "entering \""$extraAppsDir"\""
-	cd $extraAppsDir
-	echo "running git clone on Farsite \""$farsiteLink"\""
-	git clone $farsiteLink
-else
-	echo "farsiteDir \""$farsiteDir"\" already exists. Skipping repo cloning step for farsite"
+if [ $success == 0 ]; then
+	echo "" # add extra line
+	if [ ! -d "${farsiteDir}" ]; then
+		echo "entering \""$extraAppsDir"\""
+		cd $extraAppsDir
+		echo "running git clone on Farsite \""$farsiteLink"\""
+		git clone $farsiteLink
+		success=$?
+		if [ $success != 0 ]; then
+			echo "!!! error running git clone !!!"
+			success=1
+		fi
+	else
+		echo "farsiteDir \""$farsiteDir"\" already exists. Skipping repo cloning step for farsite"
+	fi
 fi
 
-echo "" # add extra line
-if [ ! -d "${windNinjaBuildDir}" ]; then
-	echo "creating windNinjaBuildDir \""$windNinjaBuildDir"\""
-	mkdir $windninjaBuildDir
-else
-	echo "windNinjaBuildDir \""$windNinjaBuildDir"\" already exists. Skipping windNinjaBuildDir creation step"
+if [ $success == 0 ]; then
+	echo "" # add extra line
+	if [ ! -d "${windNinjaBuildDir}" ]; then
+		echo "creating windNinjaBuildDir \""$windNinjaBuildDir"\""
+		mkdir $windNinjaBuildDir
+		success=$?
+		if [ $success != 0 ]; then
+			echo "!!! error running mkdir !!!"
+			success=1
+		fi
+	else
+		echo "windNinjaBuildDir \""$windNinjaBuildDir"\" already exists. Skipping windNinjaBuildDir creation step"
+	fi
+fi
+
+if [ $success == 0 ]; then
+        echo "" # add extra line
+        if [ ! -d "${finalBuildDir}" ]; then
+                echo "creating finalBuildDir \""$finalBuildDir"\""
+                mkdir $finalBuildDir
+                success=$?
+                if [ $success != 0 ]; then
+                        echo "!!! error running mkdir !!!"
+                        success=1
+                fi
+        else
+                echo "finalBuildDir \""$finalBuildDir"\" already exists. Skipping finalBuildDir creation step"
+        fi
 fi
 
 
 ### now need to load the necessary modules
-
+module load gcc/5.5.0 
 
 
 ### now attempt to build WindNinja
 
-echo "" # add extra line
-if [ ! -f "${windNinjaBuildDir}/src/cli/WindNinja_cli" ]; then
-	echo "building WindNinja"
-	cd $windNinjaBuildDir
-	sudo ldconfig
-	cmake ..
-	make -j4
-else
-	echo "WindNinja executable already exists so skipping WindNinja build process"
-fi
+#if [ $success == 0 ]; then
+#	echo "" # add extra line
+#	if [ ! -f "${windNinjaBuildDir}/src/cli/WindNinja_cli" ]; then
+#		echo "building WindNinja"
+#		cd $windNinjaBuildDir
+#		sudo ldconfig
+#		cmake ..
+#		success=$?
+#		if [ $success != 0 ]; then
+#			echo " !!! error running cmake !!!"
+#			success=1
+#		else
+#			make -j4
+#			success=$?
+#			if [ $success != 0 ]; then
+#				echo " !!! error running make !!!"
+#				success=1
+#			fi
+#		fi
+#	else
+#		echo "WindNinja executable already exists so skipping WindNinja build process"
+#	fi
+#fi
 
 
 ### now attempt to build Farsite
 
-echo "" # add extra line
-if [ ! -f "${farsiteSrcDir}/TestFARSITE" ]; then
-	echo "building Farsite"
-	cd $farsiteSrcDir
-	make -j$nCores
-else
-	echo "Farsite executable already exists so skipping Farsite build process"
+if [ $success == 0 ]; then
+	echo "" # add extra line
+	if [ ! -f "${farsiteSrcDir}/TestFARSITE" ]; then
+		echo "building Farsite"
+		cd $farsiteSrcDir
+		make -j$nCores
+		success=$?
+		if [ $success != 0 ]; then
+			echo "!!! error running make !!!"
+			success=1
+		fi
+	else
+		echo "Farsite executable already exists so skipping Farsite build process"
+	fi
 fi
 
 
@@ -165,14 +243,27 @@ fi
 
 ### now attempt to build the final overall script
 
-echo "" # add extra line
-if [ ! -f "${finalScriptDir}/bin/WRF-WindNinja-FarsiteScript" ]; then
-	echo "building WRF-WindNinja-FarsiteScript"
-	cd $finalBuildDir
-	cmake $finalScriptDir
-	make -j$nCores
-else
-	echo "WRF-WindNinja-FarsiteScript executable already exists so skipping WRF-WindNinja-FarsiteScript build process"
+if [ $success == 0 ]; then
+	echo "" # add extra line
+	if [ ! -f "${baseDir}/bin/WRF-WindNinja-FarsiteScript" ]; then
+		echo "building WRF-WindNinja-FarsiteScript"
+		cd $finalBuildDir
+		cmake $baseDir
+		success=$?
+		if [ $success != 0 ]; then
+			echo "!!! error running cmake !!!"
+			success=1
+		else
+			make -j$nCores
+			success=$?
+			if [ $success != 0 ]; then
+				echo "!!! error running make !!!"
+				success=1
+			fi
+		fi
+	else
+		echo "WRF-WindNinja-FarsiteScript executable already exists so skipping WRF-WindNinja-FarsiteScript build process"
+	fi
 fi
 
 
